61a1adaa0e67aae41240c291107e8780a64eab62
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2022-02-24 16:39:17 +0100

    fix docs

--- configs/my-tsconfig/my-tsconfig.js
+++ configs/my-tsconfig/my-tsconfig.js
@@ -1,9 +1,10 @@
 module.exports = {
+  extends: '@learn-bit-react/configs.typescript/tsconfig.json',
   compilerOptions: {
-    module: 'system',
-    noImplicitAny: true,
-    removeComments: true,
-    preserveConstEnums: true,
-    sourceMap: true
-  }
+    baseUrl: 'src' // Lets you set a base directory to resolve non-absolute module names.
+  },
+  include: [
+    'src/**/*' // *** The files TypeScript should type check ***
+  ],
+  exclude: ['node_modules', 'dist'] // *** The files to not type check ***
 };
